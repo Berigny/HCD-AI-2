9870372ab12012bd03dd77464d599602ec4e8e28
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-23 07:45:07 +0000

    dd

--- app.py
+++ app.py
@@ -3,51 +3,40 @@ import docx
 import PyPDF2
 from pptx import Presentation
 import openai
+from docx import Document
+from collections import defaultdict
 
-# Define function to handle OpenAI API calls
+# OpenAI API Call
 def query_openai(api_key, messages):
     openai.api_key = api_key
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     return response.choices[0].message["content"]
 
-# Text extraction functions for different file types
-def extract_text_from_txt(file):
-    return file.getvalue().decode('utf-8')
+# Text Extraction Functions
+# ... (retain the previous text extraction functions)
 
-def extract_text_from_docx(file):
-    doc = docx.Document(file)
-    return ' '.join([para.text for para in doc.paragraphs])
+# Analysis Function
+def analyze_text(api_key, text):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Provide a thematic analysis of the following text: {text}"}
+    ]
+    analysis_result = query_openai(api_key, messages)
+    return analysis_result  # Assume the analysis_result is structured as required
 
-def extract_text_from_pdf(file):
-    pdf_reader = PyPDF2.PdfFileReader(file)
-    text = ""
-    for page_num in range(pdf_reader.numPages):
-        text += pdf_reader.getPage(page_num).extractText()
-    return text
+# Exporting Findings
+def export_findings(analysis_result):
+    doc = Document()
+    doc.add_heading('Consolidated Findings', 0)
+    for key, value in analysis_result.items():
+        doc.add_heading(key, level=1)
+        doc.add_paragraph(value)
 
-def extract_text_from_ppt(file):
-    prs = Presentation(file)
-    text = ""
-    for slide in prs.slides:
-        for shape in slide.shapes:
-            if hasattr(shape, "text"):
-                text += shape.text
-    return text
+    doc_path = 'findings.docx'
+    doc.save(doc_path)
+    return doc_path
 
-# Function to extract text based on file type
-def extract_text(uploaded_file):
-    if uploaded_file.type == "text/plain":
-        return extract_text_from_txt(uploaded_file)
-    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        return extract_text_from_docx(uploaded_file)
-    elif uploaded_file.type == "application/pdf":
-        return extract_text_from_pdf(uploaded_file)
-    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.presentationml.presentation":
-        return extract_text_from_ppt(uploaded_file)
-    else:
-        st.error(f"File type not supported: {uploaded_file.type}")
-
-# Streamlit app UI
+# Streamlit Interface
 st.title("Transcript Analysis Tool")
 
 api_key = st.text_input("API Key", type="password")
@@ -58,16 +47,14 @@ uploaded_files = st.file_uploader(
     accept_multiple_files=True,
 )
 
-# Button to initiate the analysis
-if st.button("Submit") and uploaded_files:
+if st.button("Submit", key='submit') and uploaded_files:
     with st.spinner("Processing..."):
         # Concatenate all text from uploaded documents
         consolidated_text = " ".join([extract_text(file) for file in uploaded_files])
-
-        # Assume a function analyze_text exists to analyze the consolidated text
-        analysis_result = analyze_text(consolidated_text)
+        # Analyze the consolidated text
+        analysis_result = analyze_text(api_key, consolidated_text)
         
-        # Assume the analysis result is a dictionary with separate keys for each section
+        # Assume the analysis_result is a dictionary with separate keys for each section
         with st.expander("Summary"):
             st.write(analysis_result['summary'])
 
@@ -82,14 +69,15 @@ if st.button("Submit") and uploaded_files:
 
         with st.expander("Insights"):
             st.write(analysis_result['insights'])
+        
+        st.session_state['analysis_complete'] = True
 
-def analyze_text(text):
-    # Assume a function to analyze text and return the formatted result
-    # Replace with your actual analysis logic
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Perform a thematic analysis on the following text: {text}"}
-    ]
-    analysis = query_openai(api_key, messages)
-    # Assume the analysis is returned as a dictionary with keys corresponding to the sections
-    return analysis
+if 'analysis_complete' in st.session_state and st.button("Export Findings"):
+    doc_path = export_findings(analysis_result)
+    with open(doc_path, "rb") as file:
+        st.download_button(
+            label="Download Findings",
+            data=file.read(),
+            file_name='findings.docx',
+            mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        )
