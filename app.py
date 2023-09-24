ccd9517f21aabbd48d5613616c4909aca9817337
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-24 22:36:03 +0000

    ddfa

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def query_openai(api_key, messages):
 SEGMENT_SIZE = 3500  
 
 # Function to extract insights from text
-@st.cache_data
+@st.cache(allow_output_mutation=True)
 def extract_insights(api_key, text):
     insights = ""
     segments = text.split('. ')
@@ -42,7 +42,7 @@ def extract_insights(api_key, text):
         i += 1
     return insights.strip()
 
-@st.cache_data
+@st.cache(allow_output_mutation=True)
 def generate_summary(api_key, insight):
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
@@ -51,7 +51,6 @@ def generate_summary(api_key, insight):
     summary = query_openai(api_key, messages)
     return summary
 
-@st.cache_data
 def process_uploaded_files(uploaded_files):
     file_contents = {}
     for uploaded_file in uploaded_files:
@@ -89,15 +88,13 @@ uploaded_files = st.file_uploader(
 if st.button("Submit", key='submit') and guiding_questions and uploaded_files:
     st.session_state['file_contents'] = process_uploaded_files(uploaded_files)
     with st.spinner("Processing..."):
-        insights_expander = st.expander("Consolidated Insights & Summaries")
-        for file_name, text_content in st.session_state['file_contents'].items():
-            insights = extract_insights(api_key, text_content)
-            insights_expander.write(f"Insights from {file_name}:")
-            insights_expander.markdown(insights)
+        with st.expander("Consolidated Insights & Summaries"):
+            for file_name, text_content in st.session_state['file_contents'].items():
+                insights = extract_insights(api_key, text_content)
+                st.write(f"Insights from {file_name}:")
+                st.write(insights)
         st.session_state['processing_complete'] = True
 
         with st.expander("Summary"):
-            # Assuming that you might want to summarize insights from all files
-            all_insights = " ".join([extract_insights(api_key, text) for text in st.session_state['file_contents'].values()])
-            summary = generate_summary(api_key, all_insights)
-            st.write(summary)
+            summary = generate_summary(api_key, insights)
+            st.write(summary) 
